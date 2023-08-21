5dcae52f791addef68bea6ac0681a1a1e91b800f
sanberkzulfikar <tosanberk@icloud.com>
2023-08-21 13:18:23 +0300

    ready for deployment

---
+++ app.js
@@ -0,0 +1,19 @@
+const express = require("express");
+
+const {
+  generateMeta,
+  generateImage,
+} = require("./controllers/openaiController");
+
+//app setup
+const app = express();
+
+app.listen(4000, () => console.log("Listening for requests on port 4000"));
+
+// middleware
+app.use(express.json());
+app.use(express.static("public"));
+
+// routes
+app.post("/openai/meta", generateMeta);
+app.post("/openai/image", generateImage);
